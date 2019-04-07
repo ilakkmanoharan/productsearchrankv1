1d01327d259ff5feeda4e20238dbf23fda39e614
ilakk manoharan <ilakkmanoharan@gmail.com>
2019-04-08 01:23:39 +0530

    Update Parser.java

--- src/main/java/com/ilakk/productsearchrank/dataprocessor/Parser.java
+++ src/main/java/com/ilakk/productsearchrank/dataprocessor/Parser.java
@@ -25,7 +25,6 @@ import com.ilakk.productsearchrank.configuration.AppConfig;
 /** 
  * 
  * @author Ilakkuvaselvi Manoharan, 2019
- * 
  * @copyright GNU General Public License v3
  * No reproduction in whole or part without maintaining this copyright notice
  * and imposing this condition on any subsequent users.
