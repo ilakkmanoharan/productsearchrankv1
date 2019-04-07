643ee1eafcecee48545711c10d06dd869a754e3f
ilakk manoharan <ilakkmanoharan@gmail.com>
2019-04-08 01:25:11 +0530

    Update WordCountRanker.java

--- src/main/java/com/ilakk/productsearchrank/dataprocessor/WordCountRanker.java
+++ src/main/java/com/ilakk/productsearchrank/dataprocessor/WordCountRanker.java
@@ -20,7 +20,6 @@ import org.springframework.beans.factory.annotation.Qualifier;
 /** 
  * 
  * @author Ilakkuvaselvi Manoharan, 2019
- * 
  * @copyright GNU General Public License v3
  * No reproduction in whole or part without maintaining this copyright notice
  * and imposing this condition on any subsequent users.
