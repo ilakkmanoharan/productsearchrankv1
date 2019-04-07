9653a63f5b30c846b5dd282e0afa58ae39bc9288
ilakk manoharan <ilakkmanoharan@gmail.com>
2019-04-08 01:25:45 +0530

    Update IPSRService.java

--- src/main/java/com/ilakk/productsearchrank/service/IPSRService.java
+++ src/main/java/com/ilakk/productsearchrank/service/IPSRService.java
@@ -3,7 +3,6 @@ package com.ilakk.productsearchrank.service;
 /** 
  * 
  * @author Ilakkuvaselvi Manoharan, 2019
- * 
  * @copyright GNU General Public License v3
  * No reproduction in whole or part without maintaining this copyright notice
  * and imposing this condition on any subsequent users.
